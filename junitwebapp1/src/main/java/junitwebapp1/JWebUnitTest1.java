a4aeb52f737d995d4fc8ca17be9001e976dbab1d
HARI PRASAD <HARI PRASAD@DESKTOP-9QHAIDQ>
2017-12-13 17:33:12 +0530

    first commit to remote git

---
+++ junitwebapp1/src/main/java/junitwebapp1/JWebUnitTest1.java
@@ -0,0 +1,44 @@
+package junitwebapp1;
+
+import static net.sourceforge.jwebunit.junit.JWebUnit.assertLinkPresent;
+import static net.sourceforge.jwebunit.junit.JWebUnit.assertTitleEquals;
+import static net.sourceforge.jwebunit.junit.JWebUnit.beginAt;
+import static net.sourceforge.jwebunit.junit.JWebUnit.clickLink;
+import static net.sourceforge.jwebunit.junit.JWebUnit.setBaseUrl;
+import static net.sourceforge.jwebunit.junit.JWebUnit.setTestingEngineKey;
+
+
+import org.junit.Test;
+
+import org.junit.Before;
+
+
+import net.sourceforge.jwebunit.util.TestingEngineRegistry;
+
+public class JWebUnitTest1 {
+	
+	@Before
+	public void prepare() {
+		setTestingEngineKey(TestingEngineRegistry.TESTING_ENGINE_HTMLUNIT); 
+		setBaseUrl("http://localhost:8081/junitwebapp");
+	}
+
+	@Test
+	public void testLoginPage() {
+		beginAt("index.jsp"); 
+		assertTitleEquals("Login");
+		assertLinkPresent("home");
+		clickLink("home");
+		assertTitleEquals("Home");
+	}
+	
+	@Test
+	public void testHomePage() {
+		beginAt("home.jsp"); 
+		assertTitleEquals("Home");
+		assertLinkPresent("login");
+		clickLink("login");
+		assertTitleEquals("Login");
+	}
+
+}
