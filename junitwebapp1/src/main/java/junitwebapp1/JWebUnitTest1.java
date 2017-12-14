dbd14506a23183b89914580c7c6355680fdcf7fb
HARI PRASAD <HARI PRASAD@DESKTOP-9QHAIDQ>
2017-12-14 10:59:26 +0530

    commit class file

--- junitwebapp1/src/main/java/junitwebapp1/JWebUnitTest1.java
+++ junitwebapp1/src/main/java/junitwebapp1/JWebUnitTest1.java
@@ -1,40 +1,36 @@
 package junitwebapp1;
 
-import static net.sourceforge.jwebunit.junit.JWebUnit.assertLinkPresent;
-import static net.sourceforge.jwebunit.junit.JWebUnit.assertTitleEquals;
-import static net.sourceforge.jwebunit.junit.JWebUnit.beginAt;
-import static net.sourceforge.jwebunit.junit.JWebUnit.clickLink;
-import static net.sourceforge.jwebunit.junit.JWebUnit.setBaseUrl;
-import static net.sourceforge.jwebunit.junit.JWebUnit.setTestingEngineKey;
+import static net.sourceforge.jwebunit.junit.JWebUnit.*;
 
+import net.sourceforge.jwebunit.junit.JWebUnit.*;
 
 import org.junit.Test;
 
 import org.junit.Before;
 
-
 import net.sourceforge.jwebunit.util.TestingEngineRegistry;
 
 public class JWebUnitTest1 {
-	
+
 	@Before
 	public void prepare() {
-		setTestingEngineKey(TestingEngineRegistry.TESTING_ENGINE_HTMLUNIT); 
+
+		setTestingEngineKey(TestingEngineRegistry.TESTING_ENGINE_HTMLUNIT);
 		setBaseUrl("http://localhost:8081/junitwebapp");
 	}
 
 	@Test
 	public void testLoginPage() {
-		beginAt("index.jsp"); 
+		beginAt("index.jsp");
 		assertTitleEquals("Login");
 		assertLinkPresent("home");
 		clickLink("home");
 		assertTitleEquals("Home");
 	}
-	
+
 	@Test
 	public void testHomePage() {
-		beginAt("home.jsp"); 
+		beginAt("home.jsp");
 		assertTitleEquals("Home");
 		assertLinkPresent("login");
 		clickLink("login");
